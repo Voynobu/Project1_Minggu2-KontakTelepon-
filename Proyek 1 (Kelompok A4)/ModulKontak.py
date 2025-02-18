06c6ace65d75e5c02cdee8f2eb9fb2a0389ae374
Rangga Muhamad Fajar <rangga.m.fajar99@gmail.com>
2025-02-18 12:16:57 +0700

    Update ModulKontak.py
    Tampilkan Kontak

--- Proyek 1 (Kelompok A4)/ModulKontak.py
+++ Proyek 1 (Kelompok A4)/ModulKontak.py
@@ -1,10 +1,11 @@
 
-def DaftarKontak():
-    Buka = open("daftarkontak.txt","r")
-    print(Buka.read())
-    Buka.close()
-
 def TambahKontak(NamaNew, NomorNew):
     template = f"\n Nama : {NamaNew}, Nomor : {NomorNew}"
     Buka = open("daftarkontak.txt","a")
     Buka.write(template)
+
+def TampilKontak():
+    Buka = open("daftarkontak.txt", "r")
+    print(Buka.read())
+    Buka.close()
+    
