794767ac1aba214471f07e27a269d1d6ea34eaf3
Ivan Setiawan Ihsan <ivansetiawan2975@gmail.com>
2025-02-18 11:40:11 +0700

    Update mainKT.py

--- Proyek 1 (Kelompok A4)/mainKT.py
+++ Proyek 1 (Kelompok A4)/mainKT.py
@@ -1,24 +1,24 @@
 import os
-from ModulKontak import*
+import time
 
-os.system('cls')
-print("=======Kontak=======")
-print("1. DAFTAR KONTAK")
-print("2. TAMBAH KONTAK")
-Opsi = input("\nPilih opsi: ")
-if(Opsi == "Tambah"):
-    Tambah = True
-    while(Tambah == True):
-        NamaNew = input("Masukan nama kontak baru: ")
-        NomorNew = input("Masukan nomor kontak baru: ")
-        Verifikasi = input("Apakah informasi yang dimasukkan sudah benar? ")
-        if (Verifikasi == "Ya"):
-            Tambah = False
-    TambahKontak(NamaNew, NomorNew)      
-elif(Opsi == "Daftar"):
-    DaftarKontak()
-else:
-    print("input tidak valid")
+from ModulKontak import*
+while 1:
+    os.system("cls")
+    print("==========Kontak==========")
+    print("1. Lihat Daftar Kontak")
+    print("2. Tambah Kontak")
+    print("3. Hapus Kontak")
+    print("4. Keluar")
+
+    pilihan = int(input("\nMasukkan Pilihan "))
+    if pilihan == 1:
+        
+    elif pilihan == 2:
+
+    elif pilihan == 3:
+        
+    elif pilihan == 4:
+        break
 
 
 
