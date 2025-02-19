9957343f259bbeb49c3b75916850aa4e40f859ba
Ivan Setiawan Ihsan <ivansetiawan2975@gmail.com>
2025-02-19 13:39:42 +0700

    Update mainKT.py

--- Proyek 1 (Kelompok A4)/mainKT.py
+++ Proyek 1 (Kelompok A4)/mainKT.py
@@ -1,28 +1,50 @@
 import os
 import time
-
+import keyboard
 from ModulKontak import*
+
 while 1:
     os.system("cls")
     print("==========Kontak==========")
     print("1. Lihat Daftar Kontak")
     print("2. Tambah Kontak")
-    print("3. Hapus Kontak")
-    print("4. Cari Kontak")
+    print("3. Cari Kontak")
+    print("4. Hapus Kontak")
     print("5. Keluar")
 
-    pilihan = int(input("\nMasukkan Pilihan "))
-    if pilihan == 1:
-        
-    elif pilihan == 2:
+    pilihan = input("Masukkan pilihan: ").strip() 
+    if pilihan == "1":
+        while 1:
+            os.system("cls")
+            TampilKontak()
+            print("Tekan esc untuk kembali")
+            while not keyboard.is_pressed("esc"):
+                pass
+            break 
+               
+    elif pilihan == "2":
+            os.system("cls")
+            nama = input("Nama : ")
+            nomor = input("Nomor : ")
+            TambahKontak(nama, nomor)
+            print("Data berhasil dimasukkan")
+            time.sleep(1)
+            
+    elif pilihan == "3":
+        while 1:
+            os.system("cls")
+            nama = input("Nama : ")
+            CariKontak(nama)
+            print("Tekan esc untuk kembali")
+            while not keyboard.is_pressed("esc"):
+                pass
+            break 
+                
+    elif pilihan == "4":
+         os.system("cls")
+         nama = input("Nama : ")
+         HapusKontak(nama)
+         time.sleep(1)
 
-    elif pilihan == 3:
-
-    elif pilihan == 4:
-        
-    elif pilihan == 5:
+    elif pilihan == "5":
         break
-
-
-
-    
